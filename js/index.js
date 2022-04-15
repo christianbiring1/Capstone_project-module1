7a41efb24d08bd93b525d0a0689a986791369176
christianbiring1 <christianbiringanine22@gmail.com>
2022-04-15 10:57:46 +0200

    Add event handler on links items

--- js/index.js
+++ js/index.js
@@ -11,25 +11,29 @@ const navItem = document.querySelectorAll('.main-nav .nav-item');
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   headerLink.forEach((link) => {
-    link.classList.toggle('active')});
+    link.classList.toggle('active');
+  });
   mainNav.classList.toggle('active');
   navItem.forEach((item) => {
-    item.classList.toggle('active')});
+    item.classList.toggle('active');
+  });
   navBar.classList.toggle('active');
   header.classList.toggle('active');
   logo.classList.toggle('active');
 });
 
-headerLink.forEach((link) => { 
+headerLink.forEach((link) => {
   link.addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    navBar.classList.remove('active');
-    header.classList.remove('active');
-    headerLink.forEach((link) => {
-    link.classList.remove('active')});
-  mainNav.classList.remove('active');
-  navItem.forEach((item) => { 
-    item.classList.remove('active')});
+      hamburger.classList.remove('active');
+      navBar.classList.remove('active');
+      header.classList.remove('active');
+      headerLink.forEach((link) => {
+      link.classList.remove('active');
+    });
+    mainNav.classList.remove('active');
+    navItem.forEach((item) => { 
+      item.classList.remove('active');
+    });
   })
   logo.style.display = 'none';
 })
